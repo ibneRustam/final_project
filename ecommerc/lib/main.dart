d9b85386a26cf9bdc7293137dc4f45d2263a75f2
abdulqadeerrustam786@gmail.com <abdulqadeerrustam786@gmail.com>
2025-06-01 20:03:32 +0500

    update

--- ecommerc/lib/main.dart
+++ ecommerc/lib/main.dart
@@ -57,7 +57,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 1), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const TabbarScreen()),
